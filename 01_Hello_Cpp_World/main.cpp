d11f2a0099ec698682f7f82ad638ea1f9bf8ea62
NAVJuanan <javalledor@outlook.com>
2019-12-01 16:28:56 +0100

    Program 01 -> Hello C++ World

---
+++ 01_Hello_Cpp_World/main.cpp
@@ -0,0 +1,15 @@
+/* First program
+ * in my C++ learning
+ * path
+*/
+
+#include <iostream>
+
+using namespace std;
+
+// function main
+int main(int argc, char *argv[])
+{
+    cout << "Hello C++ world!" << endl;
+    return 0;
+}
